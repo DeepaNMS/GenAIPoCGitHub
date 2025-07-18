d8058fc4204068458a6a5f294457619fdaa7af3d
Nair <meena.deepanair@allianz.com>
2025-07-18 19:06:14 +0530

    Commit0

--- PersistenceIncident.java
+++ PersistenceIncident.java
@@ -30,7 +30,7 @@ public class PersistenceIncident extends PersistenceAuditableIdEntity {
 
     private String incidentNumber; //incident nr343kk434newdddd
     private String cfmnewincidentNumber; //incident nr4466kk
-    private String natCatCode; // nat code 66666
+    private String natCatCode; // nat code 66666kk
     private LocalDate receivedOn; //received  dd one
     private boolean isIncidentInfoOnly;
     private PersistenceAddress location;//new change
