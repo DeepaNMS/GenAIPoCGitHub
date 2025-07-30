00010b9feb93d9b07774df96cf52e62957b97780
Nair <meena.deepanair@allianz.com>
2025-07-30 13:46:06 +0530

    com1

--- PersistenceIncidentnewfile.java
+++ PersistenceIncidentnewfile.java
@@ -26,34 +26,10 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 @ExtendableClass(extensionType = ENTITY)
 @NoArgsConstructor
-public class PersistenceIncident extends PersistenceAuditableIdEntity {
+public class PersistenceIncidentnewfile extends PersistenceAuditableIdEntity {
 
-    private String incidentNumberdeepaNewfile; //incident nr
-    private String incidentNumberAdded; // new attribute new text 
-    private String incidentNumberAdded2; // Added line
-    private String natCatCode; // nat code 
-    private LocalDate receivedOnDate; // Changed line and deleted like isIncidentInfoOnly
-    private PersistenceAddress location;//new change
-    private OffsetDateTime incidentDateTime; //incidenjjjt date and time
-    @Deprecated(forRemoval = true)
-    private String whatHappenedCode; //hah
-    private String whatHappenedDescription;
-    private PersistenceCoverageData coverageData;
-    private IncidentComplexity incidentComplexity;
-    private String lossCauseGrouping;
-    private PersistenceIncidentStatus incidentStatus;
-    private PersistenceIncidentCategory incidentCategory;
-    private List<PersistenceCaseContext> relatedCases;
-    private PersistenceIncidentPolicyData policyData;
-    private List<PersistenceQuestionnaireQuestion> answeredQuestions;
-    private List<PersistenceLinkedIncident> linkedIncidents;//ff
-    private List<PersistenceContextDocument> notificationReferences;
-    private PersistenceLabeledValue reportingMedium;//dddd2323233
-    private OffsetDateTime discoveryDate; //sss2342333355444kk55
-    private List<PersistenceDocumentContextReference> additionalDocuments;
-    private boolean confidential; //cdddjj33asdadasff33eeee333
-    private PersistencePremiumStatus premiumStatus;//ddddd3322333
-    private PersistenceIncidentManuallyAddedPolicyData manuallyAddedPolicyData;
-    private String incidentTimeZone; ////dddd3377s44455554444
-    private String deepatestvariable; //sample test var test
-}
+    private String newincidentNumberdeepaNewfile; //incident test nr
+    private String newincidentNumberAdded; // new attribute new text 
+    private String newincidentNumberAdded2; // Added line
+    private String newnatCatCode; // nat code 
+ }
