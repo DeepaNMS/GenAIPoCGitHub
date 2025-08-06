21fa2d5bf88321ade20eeb717209ed394741722d
Nair <meena.deepanair@allianz.com>
2025-08-06 10:35:01 +0530

    tt

---
+++ TestFolder1/Testfolder2/Insidefolder2file2.java
@@ -0,0 +1,25 @@
+package com.allianz.bmp.claims.notification.service.core.adapter.outgoing.mongodb.notification.model.incident;
+
+import com.allianz.claims.extensions.ExtendableClass;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+import static com.allianz.claims.extensions.ExtendableClass.ENTITY;
+
+@Data
+@SuperBuilder(toBuilder = true)
+@ExtendableClass(extensionType = ENTITY)
+@NoArgsConstructor
+public class Insidefolder2file2{
+
+    private String caseNumber;
+    private boolean leadingCase;
+    private String caseLossCauseGrouping;
+    private PersistenceReserveAmount reserveAmount;
+    private String status;
+    private LocalDateTime lastReopenDateTime;
+
+}
